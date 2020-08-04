8ba9d3773de4155b462fab789fd57fb1f2000a07
unknown <galaxy.net0710@gmail.com>
2020-08-03 21:38:56 -0500

    Tetris game beta

---
+++ app.js
@@ -0,0 +1,172 @@
+document.addEventListener('DOMContentLoaded',() =>{
+ 
+    const grid = document.querySelector('.grid')
+    let squares =Array.from( document.querySelectorAll('.grid div'))
+    const scoreDisplay = document.querySelector('#score')
+    const startBtn = document.querySelector('#start-button')
+    const width=10
+
+    //the tetrominos
+    const lTretormino = [
+        [1, width+1, width*2+1,2],
+        [width,width+1,width+2,width*2+2],
+        [1,width+1,width*2+1,width*2],
+        [width,width*2,width*2+1, width*2+2]
+
+    ]
+
+    const zTetromino = [
+        [0,width,width+1,width*2+1],
+        [width+1, width+2,width*2,width*2+1],
+        [0,width,width+1,width*2+1],
+        [width+1, width+2,width*2,width*2+1]
+      ]
+    
+      const tTetromino = [
+        [1,width,width+1,width+2],
+        [1,width+1,width+2,width*2+1],
+        [width,width+1,width+2,width*2+1],
+        [1,width,width+1,width*2+1]
+      ]
+    
+      const oTetromino = [
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1]
+      ]
+    
+      const iTetromino = [
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3],
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3]
+      ]
+
+      const theTretominos = [lTretormino,zTetromino,tTetromino,oTetromino,iTetromino]
+    
+       let currentPosition = 4
+       let currentRotation= 0
+
+       //ramdomly select a tetromino and its first rotation
+      let random =Math.floor( Math.random()*theTretominos.length)
+       let current = theTretominos[random][0]
+
+       //draw the  tetromino
+      
+       function draw(){
+           current.forEach(index=>{
+               squares[currentPosition + index].classList.add('tetromino')
+           })
+       }
+
+       //draw()
+
+       //undraw the tetromino
+
+       function undraw(){
+           current.forEach(index =>{
+            squares[currentPosition + index].classList.remove('tetromino')
+           })
+       }
+
+       //make the tetromino move down every second
+
+       timerId = setInterval(moveDown,1000)
+
+       //assign keycodes
+
+       function control(e){
+           if(e.keyCode ===37){
+           moveLeft()
+       }else if (e.keyCode === 38){
+        //rotate
+        rotate()
+       }else if (e.keyCode === 39){
+        //moveRight
+        moveRight()   
+       }else if (e.keyCode === 40){
+          moveDown ()
+       }
+    }
+
+       document.addEventListener('keyup',control)
+
+       function moveDown(){
+       undraw()
+       currentPosition += width
+       draw() 
+       freeze()   
+       }
+
+       //freeze function 
+       function freeze() {
+         if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))){
+             current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+             random=Math.floor(Math.random()*theTretominos.length)
+             current= theTretominos[random][currentRotation]
+             currentPosition=4
+             draw()
+         }
+       }
+
+       function moveLeft(){
+           undraw()
+           const isAtLefEdge = current.some(index=>(currentPosition + index) % width ===0)
+           if(!isAtLefEdge) currentPosition -=1
+           if(current.some(index => squares[currentPosition + index].classList.contains('taken'))){
+               currentPosition +=1
+           }
+           draw()
+       }
+      
+       function moveRight() {
+        undraw()
+        const isAtRightEdge = current.some(index => (currentPosition + index) % width === width -1)
+        if(!isAtRightEdge) currentPosition +=1
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+          currentPosition -=1
+        }
+        draw()
+      }
+
+      ///FIX ROTATION OF TETROMINOS A THE EDGE 
+  function isAtRight() {
+    return current.some(index=> (currentPosition + index + 1) % width === 0)  
+  }
+  
+  function isAtLeft() {
+    return current.some(index=> (currentPosition + index) % width === 0)
+  }
+  
+  function checkRotatedPosition(P){
+    P = P || currentPosition       //get current position.  Then, check if the piece is near the left side.
+    if ((P+1) % width < 4) {         //add 1 because the position index can be 1 less than where the piece is (with how they are indexed).     
+      if (isAtRight()){            //use actual position to check if it's flipped over to right side
+        currentPosition += 1    //if so, add one to wrap it back around
+        checkRotatedPosition(P) //check again.  Pass position from start, since long block might need to move more.
+        }
+    }
+    else if (P % width > 5) {
+      if (isAtLeft()){
+        currentPosition -= 1
+      checkRotatedPosition(P)
+      }
+    }
+  }
+  
+  //rotate the tetromino
+  function rotate() {
+    undraw()
+    currentRotation ++
+    if(currentRotation === current.length) { //if the current rotation gets to 4, make it go back to 0
+      currentRotation = 0
+    }
+    current = theTretominos[random][currentRotation]
+    checkRotatedPosition()
+    draw()
+  }
+
+    //console.log(squares)
+
+})
